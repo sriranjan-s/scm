9d2f36c3b02ecb7ae1d5de2403790b8523e08878
soumyadip-bairagi-pwc <soumyadip.bairagi@pwc.com>
2024-12-17 18:38:27 +0530

    Resolved merge conflicts

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
@@ -11,6 +11,9 @@ import EditOrg from "./EditOrg"
 import EditDepartment from "./editDepartment";
 import ViewOffice from "./ViewOffice";
 import EditOffice from "./EditOffice";
+import Dashboard from "./Dashboard";
+import ManageOrganization from "./ViewOrganization";
+import ManageNodalUser from "./ViewNodalManagement";
 const EmployeeApp = ({ path, url, userType }) => {
   const { t } = useTranslation();
   const location = useLocation();
@@ -57,7 +60,9 @@ const EmployeeApp = ({ path, url, userType }) => {
           <PrivateRoute path={`${path}/EditOffice`} component={() => <EditOffice />} />
           <PrivateRoute path={`${path}/appeal-dashboard`} component={() => <AppealDashboard />} />
           <PrivateRoute path={`${path}/grievance-details/:grievanceId/:appealId`} component={() => <GrievanceDetailsPage />} />
-          
+          <PrivateRoute path={`${path}/viewwOrg`} component={() => <ManageOrganization />} />
+          <PrivateRoute path={`${path}/viewwUser`} component={() => <ManageNodalUser />} />
+          <PrivateRoute path={`${path}/Dashboard`} component={() => <Dashboard />} />
           <PrivateRoute path={`${path}/response`} component={(props) => <HRMSResponse {...props} parentRoute={path} />} />
           <PrivateRoute path={`${path}/details/:tenantId/:id`} component={() => <HRMSDetails />} />
           <PrivateRoute path={`${path}/edit/:tenantId/:id`} component={() => <EditEmpolyee />} />
